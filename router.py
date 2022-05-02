12c36495c1d5a0e4ccbe11e8cae28f484e652c30
lleans <ahmadunnail@gmail.com>
2022-05-02 16:53:28 +0700

    🐛 fix some bug

--- router.py
+++ router.py
@@ -1,6 +1,6 @@
 from aiohttp import ClientSession
 from MuslimProAPI import Search
-from flask import Flask, jsonify, redirect
+from flask import Flask, jsonify, redirect, abort
 from os import environ
 
 app = Flask('MuslimPro_API')
@@ -11,20 +11,25 @@ app.config['JSON_SORT_KEYS'] = False
 @app.route('/<string:query>', methods=['GET'])
 async def main_app(query: str):
     response = {}
-    async with ClientSession() as session:
-        api = Search(session=session)
-        data = await api.search(location=query)
-    
-    response['location'] = query.capitalize()
-    response['date'] = "Today"
-    response['praytimes'] = {}
-    for i in range(6):
-        response['praytimes'][data.raw[i].prayers] = data.raw[i].praytime
-    
-    return jsonify(response)
+    query = query.strip()
+    if query != "":
+        async with ClientSession() as session:
+            api = Search(session=session)
+            data = await api.search(location=query)
+        
+        response['location'] = query.capitalize()
+        response['date'] = "Today"
+        response['praytimes'] = {}
+        for i in range(6):
+            response['praytimes'][data.raw[i].prayers] = data.raw[i].praytime
+        
+        return jsonify(response)
+    else:
+        abort(400)
 
+@app.errorhandler(400)
 @app.errorhandler(404)
-async def redirect():
-    return redirect('https://github.com/lleans/Muslim-Pro-Scrapper', code=400)
+async def docs(e):
+    return redirect('https://github.com/lleans/Muslim-Pro-Scrapper')
 
 app.run(port=environ.get('PORT') or 8000, host='0.0.0.0')
