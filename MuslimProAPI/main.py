a587132710e248dca21d4809ab897f274d274a17
lleans <ahmadunnail@gmail.com>
2022-05-07 11:27:04 +0700

    ✨ New Feature, Read README

--- MuslimProAPI/main.py
+++ MuslimProAPI/main.py
@@ -1,24 +1,24 @@
-
-import aiohttp
-
+from aiohttp import ClientSession
 from lxml.html import HTMLParser, fromstring
 from pyquery import PyQuery
 
 from .model import Response
-from .const import MUSLIMPRO_URL, GEOCODE_URL
+from .const import MUSLIMPRO_URL, GEOCODE_URL, AsrjuristicMethod, CalculationMethod
 
 
 class Search:
-    def __init__(self, session=None, *, lib='asyncio', loop=None, **request_kwargs):
+    def __init__(self, session=None, *, lib='asyncio', loop=None, calculation: str = CalculationMethod.DEFAULT.value, asrjuristic: str = AsrjuristicMethod.STANDARD_SHAFI_MALIKI_HANBALI.value, **request_kwargs):
         self.request_kwargs = request_kwargs
+        self.calculation = calculation
+        self.asrjuristic = asrjuristic
         if lib not in ('asyncio'):
             raise ValueError(
                 f"lib must be of type `str` and be either `asyncio' not '{lib if isinstance(lib, str) else lib.__class__.__name__}'")
         self._lib = lib
         if lib == 'asyncio':
-            import asyncio
-            loop = loop or asyncio.get_event_loop()
-        self.session = session or aiohttp.ClientSession(loop=loop)
+            from asyncio import get_event_loop
+            loop = loop or get_event_loop()
+        self.session = session or ClientSession(loop=loop)
 
     @staticmethod
     def _errors(code):
@@ -27,7 +27,7 @@ class Search:
         elif code == 302:
             return "Moved temporarily, or blocked by captcha"
         elif code == 403:
-            return "Forbidden,or token unvalid"
+            return "Forbidden,or unvalid"
         elif code == 429:
             return "Too many request"
         elif code == 500:
@@ -36,10 +36,11 @@ class Search:
             return "Unknown error, please report to the project maintainer"
 
     @staticmethod
-    def _slice(resp: str) -> Response:
+    def _slice(self, resp: str) -> Response:
         utf8_parser = HTMLParser(encoding='utf-8')
-        data = PyQuery(fromstring(resp, parser=utf8_parser))('.box-prayer')
-        return Response(data=data)
+        data = PyQuery(fromstring(resp, parser=utf8_parser))(
+            'tbody').eq(0)('tr')
+        return Response(data=data, calcMethod=self.calculation, asrjurMethod=self.asrjuristic)
 
     async def geocode(self, location: str):
         params = {
@@ -47,14 +48,17 @@ class Search:
         }
         response = await self.session.get(GEOCODE_URL, params=params, **self.request_kwargs)
         data = await response.json()
-        return {
+        data = {
             'country_code': data['data'][0]['country_module']['global']['alpha2'],
             'country_name': data['data'][0]['country'],
             'city_name': data['data'][0]['name'],
-            'coordinates': f"{data['data'][0]['latitude']},{data['data'][0]['longitude']}"
+            'coordinates': f"{data['data'][0]['latitude']},{data['data'][0]['longitude']}",
+            'convention': self.calculation,
+            'asrjuristic': self.asrjuristic
         } if response.status == 200 else Exception(self._errors(response.status))
+        return data
 
     async def search(self, location: str):
         geocode = await self.geocode(location=location)
         response = await self.session.get(MUSLIMPRO_URL, params=geocode, **self.request_kwargs)
-        return self._slice(resp=await response.text()) if response.status == 200 else Exception(self._errors(response.status))
+        return self._slice(self, resp=await response.text()) if response.status == 200 else Exception(self._errors(response.status))
--- router.py
+++ router.py
@@ -1,7 +1,9 @@
 from aiohttp import ClientSession
 from MuslimProAPI import Search
-from flask import Flask, jsonify, redirect, abort
+from flask import Flask, jsonify, redirect, abort, request
 from os import environ
+
+from MuslimProAPI.const import AsrjuristicMethod, CalculationMethod
 
 app = Flask('MuslimPro_API')
 
@@ -13,15 +15,20 @@ async def main_app(query: str):
     response = {}
     query = query.strip()
     if query != "":
+        par1, par2 = str(request.args.get('calcMethod')).replace(' ', '_').upper(), str(request.args.get('asjurMethod')).replace(' ', '_').upper()
+        calcMethod = CalculationMethod[par1].value if hasattr(CalculationMethod, par1) else CalculationMethod.DEFAULT.value
+        asrjurMethod = AsrjuristicMethod[par2].value if hasattr(AsrjuristicMethod, par2) else AsrjuristicMethod.STANDARD_SHAFI_MALIKI_HANBALI.value
         async with ClientSession() as session:
-            api = Search(session=session)
+            api = Search(session=session, calculation=calcMethod, asrjuristic=asrjurMethod)
             data = await api.search(location=query)
-        
-        response['location'] = query.capitalize()
-        response['date'] = "Today"
+            session.close()
+
+        response['location'] = query.title()
+        response['calculationMethod'] = data.calculationMethod
+        response['asrjuristicMethod'] = data.asrjuristicMethod
         response['praytimes'] = {}
-        for i in range(6):
-            response['praytimes'][data.raw[i].prayers] = data.raw[i].praytime
+        for i in iter(data.raw):
+            response['praytimes'][i.date] = i.prayertimes
         
         return jsonify(response)
     else:
@@ -32,4 +39,4 @@ async def main_app(query: str):
 async def docs(e):
     return redirect('https://github.com/lleans/Muslim-Pro-Scrapper')
 
-app.run(port=environ.get('PORT') or 8000, host='0.0.0.0')
+app.run(port=environ.get('PORT') or 8000, host='0.0.0.0', threaded=False)
