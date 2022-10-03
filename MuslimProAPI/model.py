dc06dcbf07dc11d78963dd2007ecdb3e48a4d3a0
lleans <ahmadunnail@gmail.com>
2022-10-03 23:46:35 +0700

    ✨ New Feature, Read README

--- MuslimProAPI/model.py
+++ MuslimProAPI/model.py
@@ -1,16 +1,24 @@
+from itertools import islice, cycle
 from typing import List
 from pyquery import PyQuery
+from .const import CalculationMethod, AsrjuristicMethod
+
 
 class Item:
-    def __init__(self, data: PyQuery):
-        self.origin: PyQuery = data
-        self.prayers: str = data('p').eq(0).text()
-        self.praytime: str = data('p').eq(1).text()
+    def __init__(self, data, prayers: PyQuery):
+        self.date = data('.prayertime-1').eq(0).text()
+        items = zip(prayers, data('.prayertime').items())
+        self.prayertimes: dict = dict([(k.text(), v.text()) for k, v in items])
 
     def __repr__(self):
-        return f'<Praytime(prayers={repr(self.prayers)}, praytime={self.praytime})>'
+        return f'<Praytime(date={repr(self.date)}, praytimes={self.prayertimes})>'
+
 
 class Response:
-    def __init__(self, data: PyQuery):
-        self.origin:PyQuery = data
-        self.raw: List[Item] = [Item(i) for i in data.items()]
+    def __init__(self, data: PyQuery, calcMethod, asrjurMethod: str):
+        self.origin: PyQuery = data
+        self.calculationMethod: str = CalculationMethod(calcMethod).name
+        self.asrjuristicMethod:str = AsrjuristicMethod(asrjurMethod).name
+        prayers = islice(cycle(data('th').items()), 1, None)
+        self.raw: List[Item] = [Item(i, prayers)
+                                for i in islice(data.items(), 1, None)]
--- router.py
+++ router.py
@@ -1,35 +1,60 @@
 from aiohttp import ClientSession
 from MuslimProAPI import Search
-from flask import Flask, jsonify, redirect, abort
+from flask import Flask, jsonify, redirect, request
 from os import environ
+
+from MuslimProAPI.const import AsrjuristicMethod, CalculationMethod
 
 app = Flask('MuslimPro_API')
 
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['JSON_SORT_KEYS'] = False
 
+async def where_ip(session, ip_address: str):
+    url = "https://demo.ip-api.com/json/"
+    headers = {
+        "Origin": "https://ip-api.com",
+        "Referer": "https://ip-api.com"
+    }
+    params = {
+        "fields": environ.get("API_KEY") or open("API_KEY").readline(),
+        "lang": "en"
+    }
+    response = await session.get(url+ip_address, params=params, headers=headers)
+    response = await response.json()
+    return f"{response['city']}, {response['regionName']}, {response['country']}, {response['continent']}"
+
 @app.route('/<string:query>', methods=['GET'])
-async def main_app(query: str):
+@app.route('/', methods=['GET'])
+async def main_app(query: str = None):
     response = {}
-    query = query.strip()
-    if query != "":
-        async with ClientSession() as session:
-            api = Search(session=session)
-            data = await api.search(location=query)
-        
-        response['location'] = query.capitalize()
-        response['date'] = "Today"
-        response['praytimes'] = {}
-        for i in range(6):
-            response['praytimes'][data.raw[i].prayers] = data.raw[i].praytime
-        
-        return jsonify(response)
-    else:
-        abort(400)
+
+    par1, par2 = str(request.args.get('calcMethod')).replace(' ', '_').upper(), str(request.args.get('asjurMethod')).replace(' ', '_').upper()
+    calcMethod = CalculationMethod[par1].value if hasattr(CalculationMethod, par1) else CalculationMethod.DEFAULT.value
+    asrjurMethod = AsrjuristicMethod[par2].value if hasattr(AsrjuristicMethod, par2) else AsrjuristicMethod.STANDARD_SHAFI_MALIKI_HANBALI.value
+    async with ClientSession() as session:
+        api = Search(session=session, calculation=calcMethod, asrjuristic=asrjurMethod)
+        if query is None:
+            list_ip = request.headers['x-forwarded-for'].split(',')
+            query = await where_ip(session=session, ip_address=list_ip[len(list_ip)-1] or request.remote_addr)
+        else:
+            query = query.strip()
+        data = await api.search(location=query)
+        session.close()
+
+    response['location'] = query.title()
+    response['calculationMethod'] = data.calculationMethod
+    response['asrjuristicMethod'] = data.asrjuristicMethod
+    response['praytimes'] = {}
+    for i in iter(data.raw):
+        response['praytimes'][i.date] = i.prayertimes
+    
+    return jsonify(response)
+    
 
 @app.errorhandler(400)
 @app.errorhandler(404)
 async def docs(e):
     return redirect('https://github.com/lleans/Muslim-Pro-Scrapper')
 
-app.run(port=environ.get('PORT') or 8000, host='0.0.0.0')
+app.run(port=environ.get('PORT') or 8000, host='0.0.0.0', threaded=False)
