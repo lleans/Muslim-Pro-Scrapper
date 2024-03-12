3de8327523241b60317e7cd2c32e944d381b0668
lleans <ahmadunnail@gmail.com>
2024-03-12 22:06:19 +0700

    ✨ Migrate to FastAPI

--- router.py
+++ router.py
@@ -1,22 +1,34 @@
-from aiohttp import ClientSession
-from flask import Flask, jsonify, redirect, request, url_for
-from flask_caching import Cache
 from os import environ
+from contextlib import asynccontextmanager
 
-from MuslimProScrapper import Search
+from aiohttp import ClientSession
+
+from pydantic import BaseModel, Field
+
+from fastapi import FastAPI, Request
+from fastapi.responses import FileResponse, JSONResponse, RedirectResponse
+from fastapi.encoders import jsonable_encoder
+from fastapi.staticfiles import StaticFiles
+
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from fastapi_cache.decorator import cache
+
+from redis import asyncio as aioredis
+
+import uvicorn
+
+from MuslimProScrapper import Search, Response as MuslimProResp
 from MuslimProScrapper.const import AsrjuristicMethod, CalculationMethod
 
-config: dict = {
-    'CORS_HEADERS': 'Content-Type',
-    'JSON_SORT_KEYS': False,
-    'CACHE_TYPE': 'FileSystemCache',
-    "CACHE_DEFAULT_TIMEOUT": 86400,
-    "CACHE_DIR": "cache"
-}
+@asynccontextmanager
+async def lifespan(_):
+    redis = aioredis.from_url(f"redis://:{environ.get('REDIS_PASS')}@redis")
+    FastAPICache.init(RedisBackend(redis), prefix="muslimproapi-cache")
+    yield
 
-app = Flask('MuslimPro_API')
-app.config.from_mapping(config)
-cache: Cache = Cache(app)
+app = FastAPI(title='MuslimPro_API', lifespan=lifespan)
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 async def where_ip(session: ClientSession, ip_address: str) -> str:
@@ -36,101 +48,100 @@ async def where_ip(session: ClientSession, ip_address: str) -> str:
         return f"{res['city']}, {res['regionName']}, {res['country']}, {res['continent']}"
 
 
-@app.route('/favicon.ico')
-def favicon():
-    return url_for('static', filename='favicon.ico')
+class ModelResponse(BaseModel):
+    location: str = Field(default="Location not found!!")
+    calculationMethod: str = Field(default="")
+    asrjuristicMethod: str = Field(default="")
+    praytimes: dict = Field(default=dict())
+    ramadhan: dict | str = Field(default="")
 
 
-@app.route('/', methods=['GET'])
-async def get_location_based():
-    not_found = 'Location not Found'
-    response = {
-        'location': "",
-        'calculationMethod': "",
-        'asrjuristicMethod': "",
-        'praytimes': "",
-        'ramadhan': ""
-    }
-
-    calcMethod = CalculationMethod.DEFAULT
-    asrjurMethod = AsrjuristicMethod.STANDARD_SHAFI_MALIKI_HANBALI
-
-    async with ClientSession() as session:
-        api = Search(session=session, calculation=calcMethod,
-                     asrjuristic=asrjurMethod)
-        try:
-            list_ip = request.headers['x-forwarded-for'].split(',')
-            query: str = await where_ip(session=session, ip_address=list_ip[len(list_ip)-1] or request.remote_addr)
-
-            data = await api.search(location=query)
-            location = await api.geocode(location=query)
-
-            if location['country_name'].title() == "Indonesia":
-                response['ramadhan'] = await api.ramadhan_time()
-            else:
-                response['ramadhan'] = "Currently only supported in Indonesian"
-        except (IndexError, KeyError):
-            response['location'] = not_found
-
-    if response['location'] != not_found:
-        response['location'] = f"{location['city_name'].title()}, {location['country_name'].title()}"
-        response['calculationMethod'] = data.calculationMethod
-        response['asrjuristicMethod'] = data.asrjuristicMethod
-        response['praytimes'] = {}
-        for i in iter(data.raw):
-            response['praytimes'][i.date] = i.prayertimes
-
-    return jsonify(response)
+class BaseResponse(BaseModel):
+    status_code: int = Field(default=200)
+    message: str = Field(default="OK")
+    data: ModelResponse = Field(default=ModelResponse())
 
 
-@app.route('/<string:query>', methods=['GET'])
-@cache.cached(query_string=True)
-async def main_app(query: str = None):
-    not_found = 'Location not Found'
-    response = {
-        'location': "",
-        'calculationMethod': "",
-        'asrjuristicMethod': "",
-        'praytimes': "",
-        'ramadhan': ""
-    }
+@app.get('/favicon.ico')
+def favicon() -> FileResponse:
+    return FileResponse("static/favicon.ico")
 
-    par1, par2 = str(request.args.get('calcMethod')).replace(' ', '_').upper(), str(
-        request.args.get('asjurMethod')).replace(' ', '_').upper()
-    calcMethod = CalculationMethod[par1] if hasattr(
+
+@app.get('/', response_model=None)
+async def get_location_based(request: Request) -> RedirectResponse | JSONResponse:
+    resp: BaseResponse = BaseResponse()
+    try:
+        async with ClientSession() as session:
+            client_ip: str = request.client.host
+            location: str = await where_ip(session=session, ip_address=client_ip)
+
+            redirect_url = request.url_for('main_app', **{'query': location})
+            return RedirectResponse(redirect_url)
+
+    except (IndexError, KeyError):
+        resp.status_code = 404
+        return JSONResponse(content=jsonable_encoder(resp), status_code=404)
+    except:
+        resp.message = "Something went wrong"
+        resp.status_code = 500
+        resp.data.location = ""
+        return JSONResponse(content=jsonable_encoder(resp), status_code=500)
+
+
+@app.get('/{query}', response_class=JSONResponse)
+@cache(expire=86400)
+async def main_app(query: str, calcMethod: str = "", asjurMethod: str = "") -> JSONResponse:
+    resp: BaseResponse = BaseResponse()
+    status: int = 200
+
+    par1, par2 = calcMethod.replace(
+        ' ', '_').upper(), asjurMethod.replace(' ', '_').upper()
+
+    calc = CalculationMethod[par1] if hasattr(
         CalculationMethod, par1) else CalculationMethod.DEFAULT
-    asrjurMethod = AsrjuristicMethod[par2] if hasattr(
+    asjur = AsrjuristicMethod[par2] if hasattr(
         AsrjuristicMethod, par2) else AsrjuristicMethod.STANDARD_SHAFI_MALIKI_HANBALI
     async with ClientSession() as session:
-        api = Search(session=session, calculation=calcMethod,
-                     asrjuristic=asrjurMethod)
+        api = Search(session=session, calculation=calc,
+                     asrjuristic=asjur)
         try:
             query = query.strip()
 
-            data = await api.search(location=query)
-            location = await api.geocode(location=query)
+            data: MuslimProResp = await api.search(location=query)
+            location: dict = await api.geocode(location=query)
 
-            if location['country_name'].title() == "Indonesia":
-                response['ramadhan'] = await api.ramadhan_time()
-            else:
-                response['ramadhan'] = "Currently only supported in Indonesian"
+            if location:
+                city_name: str = location.get('city_name', '')
+                country_name: str = location.get('country_name', '')
+
+                resp.data.location = f"{city_name.title()}, {country_name.title()}"
+                resp.data.calculationMethod = data.calculationMethod
+                resp.data.asrjuristicMethod = data.asrjuristicMethod
+                for i in iter(data.raw):
+                    resp.data.praytimes.update({
+                        i.date: i.prayertimes
+                    })
+
+                if country_name.title() == "Indonesia":
+                    resp.data.ramadhan = await api.ramadhan_time()
+                else:
+                    resp.data.ramadhan = "Currently only supported in Indonesia"
+
         except (IndexError, KeyError):
-            response['location'] = not_found
+            status = 404
+            resp.status_code = status
+        except:
+            status = 500
+            resp.message = "Something went wrong"
+            resp.status_code = status
+            resp.data.location = ""
 
-    if response['location'] != not_found:
-        response['location'] = f"{location['city_name'].title()}, {location['country_name'].title()}"
-        response['calculationMethod'] = data.calculationMethod
-        response['asrjuristicMethod'] = data.asrjuristicMethod
-        response['praytimes'] = {}
-        for i in iter(data.raw):
-            response['praytimes'][i.date] = i.prayertimes
-
-    return jsonify(response)
+    return JSONResponse(content=jsonable_encoder(resp), status_code=status)
 
 
-@app.errorhandler(400)
-@app.errorhandler(404)
+@app.exception_handler(400)
+@app.exception_handler(404)
 async def docs(e):
-    return redirect('https://github.com/lleans/Muslim-Pro-Scrapper')
+    return RedirectResponse('https://github.com/lleans/Muslim-Pro-Scrapper')
 
-app.run(port=environ.get('PORT') or 8000, host='0.0.0.0', threaded=False)
+uvicorn.run("router:app", port=environ.get('PORT') or 8000, workers=4)
